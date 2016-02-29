dc521ecd899f651db1c3aa8a44f1f4d010343def
JP <JP@Julio>
2016-02-29 09:56:21 +0100

    parte desarrolador A

---
+++ src/colaboracionramas/ColaboracionRamas.java
@@ -0,0 +1,19 @@
+
+package colaboracionramas;
+
+/**
+ *
+ * @author jpatriciodasilva
+ */
+public class ColaboracionRamas {
+
+   
+    public static void main(String[] args) {
+ 
+        MetodosSuma sum1 =new MetodosSuma();
+        
+        
+       sum1.suma();
+    }
+    
+}
