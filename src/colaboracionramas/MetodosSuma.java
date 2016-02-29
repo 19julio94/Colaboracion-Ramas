32aa9220d434d5d526efb77846d07b65e3759383
JP <JP@Julio>
2016-02-29 09:24:37 +0100

    Primer parte desarrollador 1 terminada

---
+++ src/colaboracionramas/MetodosSuma.java
@@ -0,0 +1,29 @@
+
+package colaboracionramas;
+
+import javax.swing.JOptionPane;
+
+
+public class MetodosSuma {
+ private int num1,num2;
+ 
+    public void suma(){
+      num1=Integer.parseInt(JOptionPane.showInputDialog("introduce o numero"));
+      num2=Integer.parseInt(JOptionPane.showInputDialog("introduce o numero"));
+      
+      System.out.println("O resultado e: "+(num1+num2));
+        
+        
+    }
+    
+
+   
+      
+   
+   
+   
+   
+   
+   }
+    
+
